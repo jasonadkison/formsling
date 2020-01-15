4d14b271c8dff426684d4b651fe20b043c6f5169
Jason Adkison <jadkison@gmail.com>
2020-01-15 17:20:51 -0600

    adds bulma variables and customizes devise views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def css_class_for_flash_type(type)
+    case type
+    when 'success'
+      'is-success'
+    when 'error'
+      'is-danger'
+    when 'alert'
+      'is-warning'
+    when 'notice'
+      'is-info'
+    else
+      type.to_s
+    end
+  end
 end
