245e35ecc62a846c68739237abdf0b11b270b4df
Jason Adkison <jadkison@gmail.com>
2020-01-15 13:22:17 -0600

    adds devise basic user auth; moves react app to authenticated endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   namespace :api, defaults: { format: :json } do
 
   end
 
   # Forward all HTML (non-XHR) requests to StaticController#index
-  get '*page', to: 'static#index', constraints: ->(req) { !req.xhr? && req.format.html? }
+  get '/app/*page', to: 'dashboard#index', constraints: ->(req) { !req.xhr? && req.format.html? }
+  get '/app', to: 'dashboard#index', as: 'app'
 
-  root 'static#index'
+  root 'welcome#index'
 end
