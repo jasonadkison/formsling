cbcbdcbae93e2f7891423314097fa21323268bf3
Jason Adkison <jadkison@gmail.com>
2020-01-18 03:48:04 -0600

    simplify form editor breadcrumbs

--- app/javascript/components/FormEditor/Breadcrumbs.js
+++ app/javascript/components/FormEditor/Breadcrumbs.js
@@ -2,16 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const Breadcrumbs = ({ id }) => (
+const Breadcrumbs = ({ id, name }) => (
   <ul>
-    <li>
-      <Link to="/app">Dashboard</Link>
-    </li>
     <li>
       <Link to="/app">Forms</Link>
     </li>
     <li className="is-active">
-      <Link to={`/forms/${id}/edit`}>Edit Form</Link>
+      <Link to={`/forms/${id}/edit`}>{name}</Link>
     </li>
   </ul>
 );
