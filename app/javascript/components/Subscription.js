bebd0bbfa3696dc3fc79fc522ada427a42471959
Jason Adkison <jadkison@gmail.com>
2020-01-18 05:50:23 -0600

    adds the subscription page mockup

--- app/javascript/components/Subscription.js
+++ app/javascript/components/Subscription.js
@@ -6,6 +6,49 @@ const Subscription = () => (
       <h1>Subscription</h1>
       <p>Welcome to your subscription.</p>
     </div>
+    <div className="pricing-table">
+      <div className="pricing-plan">
+        <div className="plan-header">Free</div>
+        <div className="plan-price"><span className="plan-price-amount"><span className="plan-price-currency">$</span>0</span>/month</div>
+        <div className="plan-items">
+          <div className="plan-item">10 Forms</div>
+          <div className="plan-item">100 Monthly Results</div>
+          <div className="plan-item">-</div>
+          <div className="plan-item">-</div>
+        </div>
+        <div className="plan-footer">
+          <button className="button is-fullwidth" disabled="disabled">Current plan</button>
+        </div>
+      </div>
+
+      <div className="pricing-plan is-success is-active">
+        <div className="plan-header">Starter</div>
+        <div className="plan-price"><span className="plan-price-amount"><span className="plan-price-currency">$</span>10</span>/month</div>
+        <div className="plan-items">
+          <div className="plan-item">Unlimited Forms</div>
+          <div className="plan-item">1000 Monthly Results </div>
+          <div className="plan-item">Some Perk</div>
+          <div className="plan-item">-</div>
+        </div>
+        <div className="plan-footer">
+          <button className="button is-fullwidth">Choose</button>
+        </div>
+      </div>
+
+      <div className="pricing-plan is-dark">
+        <div className="plan-header">Professional</div>
+        <div className="plan-price"><span className="plan-price-amount"><span className="plan-price-currency">$</span>40</span>/month</div>
+        <div className="plan-items">
+          <div className="plan-item">Unlimited Forms</div>
+          <div className="plan-item">Unlimited Results</div>
+          <div className="plan-item">Some Perk</div>
+          <div className="plan-item">Some Perk</div>
+        </div>
+        <div className="plan-footer">
+          <button className="button is-fullwidth">Choose</button>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
