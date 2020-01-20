fd8f28f90df1c8fd5c1c2f64c78f0b1ba223fc6d
Jason Adkison <jadkison@gmail.com>
2020-01-20 02:41:25 -0600

    remove dummy text

--- app/javascript/components/FormEditor/ColumnsProperties.js
+++ app/javascript/components/FormEditor/ColumnsProperties.js
@@ -1,20 +1,5 @@
 import React, { useState } from 'react';
-import { useNode } from '@craftjs/core';
-import Property from './Property';
 
-const ColumnsProperties = () => {
-  const {
-    setProp,
-    totalColumns,
-  } = useNode(node => node.data.props);
-
-  return (
-    <div className="panel-block">
-      <Property>
-        <p>Columns don't have any properties to manage.</p>
-      </Property>
-    </div>
-  );
-};
+const ColumnsProperties = () => null;
 
 export default ColumnsProperties;
