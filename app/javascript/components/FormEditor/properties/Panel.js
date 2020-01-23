ca0db5dd76c9a039a10b465dd008190c9dc73731
Jason Adkison <jadkison@gmail.com>
2020-01-23 17:20:54 -0600

    only render label if populated

--- app/javascript/components/FormEditor/properties/Panel.js
+++ app/javascript/components/FormEditor/properties/Panel.js
@@ -4,9 +4,11 @@ import PropTypes from 'prop-types';
 const Panel = ({ label, children }) => (
   <div className="panel-block">
     <div className="control">
-      <label className="label is-small">
-        {label}
-      </label>
+      {label && (
+        <label className="label is-small">
+          {label}
+        </label>
+      )}
       <div className="control">
         {children}
       </div>
