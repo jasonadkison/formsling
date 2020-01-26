b0aa3228fd6f65b360c69a0206c72bd59db61804
Jason Adkison <jadkison@gmail.com>
2020-01-25 20:26:33 -0600

    adds http_basic_authenticate_with for preventing access to staging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
-
+  if ENV["DISALLOW_ALL_WEB_CRAWLERS"].present?
+    http_basic_authenticate_with(
+      name: ENV.fetch("BASIC_AUTH_USERNAME"),
+      password: ENV.fetch("BASIC_AUTH_PASSWORD"),
+    )
+  end
 end
