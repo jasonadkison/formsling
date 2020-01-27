22122ce117f46dd3535aa7c4de6348ee3641817c
Jason Adkison <jadkison@gmail.com>
2020-01-27 10:50:42 -0600

    adds onload script to make result snapshot fields readonly

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("@rails/activestorage").start()
 require("channels")
 
 import '../stylesheets/main';
+import prepareSnapshotDisplay from '../lib/prepare_snapshot_display';
+prepareSnapshotDisplay();
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
