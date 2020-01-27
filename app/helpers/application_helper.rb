ce57038161028aff1c3a26c189fe419f15e11652
Jason Adkison <jadkison@gmail.com>
2020-01-26 22:21:48 -0600

    testing fix for production wicked_pdf exception

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,14 @@ module ApplicationHelper
       type.to_s
     end
   end
+
+  def production_pdf_pack_tag(asset)
+    file_path = Rails.root.join('public', asset_pack_path(asset))
+    content = File.read(file_path)
+    if asset.include?('.js')
+      "<script type='text/javascript'>#{content}</script>".html_safe
+    else
+      "<style type='text/css'>#{content}</style>".html_safe
+    end
+  end
 end
