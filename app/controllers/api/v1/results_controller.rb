8834a3154b02ac033e94cd004fd29bf024c8134a
Jason Adkison <jadkison@gmail.com>
2020-01-27 10:49:55 -0600

    order results

--- app/controllers/api/v1/results_controller.rb
+++ app/controllers/api/v1/results_controller.rb
@@ -3,7 +3,7 @@ module Api
     class ResultsController < Api::Controller
       def index
         relation = current_user.admin? ? Form.all : current_user.forms
-        render json: relation.find(params[:form_id]).results
+        render json: relation.find(params[:form_id]).results.order(created_at: :desc)
       end
     end
   end
