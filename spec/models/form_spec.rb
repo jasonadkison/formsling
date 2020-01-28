fb5cc4328d108d8058bc919022dc0da128f60aaf
Jason Adkison <jadkison@gmail.com>
2020-01-28 13:45:16 -0600

    test default form payload is set

--- spec/models/form_spec.rb
+++ spec/models/form_spec.rb
@@ -8,9 +8,15 @@ describe Form do
       expect(form.errors.full_messages).to match_array([
         "Name can't be blank",
         "User can't be blank",
-        "User must exist",
-        "Payload can't be blank"
+        "User must exist"
       ])
+    end
+  end
+
+  context 'defaults' do
+    it 'sets the default payload' do
+      form = Form.new
+      expect(form.payload.present?).to be_truthy
     end
   end
 end
