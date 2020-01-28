e25ca78f8f21d1f04cb7b3be062ff0a6ab18e287
Jason Adkison <jadkison@gmail.com>
2020-01-27 22:17:33 -0600

    tweaks pdf rendering / adds header and footer

--- app/lib/render_pdf.rb
+++ app/lib/render_pdf.rb
@@ -1,5 +1,21 @@
 class RenderPdf
   def self.for_result(result, renderer: WickedPdf.new)
-    renderer.send(:pdf_from_string, result.pdf_html)
+    options = {
+      header: {
+        left: result.form.name,
+        center: "FormSling.com Generated Document",
+        right: '[page] of [topage]',
+        font_size: 10
+      },
+      footer: {
+        left: result.form.name,
+        center: "FormSling.com Generated Document",
+        right: '[page] of [topage]',
+        font_size: 10
+      },
+      grayscale: true,
+      extra: '--no-print-media-type --disable-forms --disable-javascript'
+    }
+    renderer.send(:pdf_from_string, result.pdf_html, options)
   end
 end
