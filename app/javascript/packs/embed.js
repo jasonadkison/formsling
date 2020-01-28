1c8675bf8d2e17a85717769bee504028ef5aa91d
Jason Adkison <jadkison@gmail.com>
2020-01-28 16:42:21 -0600

    removes width/height from iframe; adds iframe class

--- app/javascript/packs/embed.js
+++ app/javascript/packs/embed.js
@@ -4,9 +4,8 @@ $(document).ready(function() {
   var iframe;
   $('.formsling-form-widget').each(function (i, link) {
     iframe = document.createElement('iframe');
+    iframe.classList.add('formsling-iframe');
     iframe.setAttribute('src', link.href);
-    iframe.setAttribute('width', '100%');
-    iframe.setAttribute('height', '100%');
     iframe.setAttribute('frameborder', '0');
     iframe.setAttribute('scrolling', 'yes');
     link.parentNode.replaceChild(iframe, link);
