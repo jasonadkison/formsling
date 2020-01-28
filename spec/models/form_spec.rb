aac2876dc4347a3d148fa05b08e8447619529b06
Jason Adkison <jadkison@gmail.com>
2020-01-28 13:56:30 -0600

    test #email_recipient

--- spec/models/form_spec.rb
+++ spec/models/form_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 describe Form do
+  subject { Form.new }
+
   context 'validations' do
     it 'validates attributes' do
-      form = Form.new
-      form.valid?
-      expect(form.errors.full_messages).to match_array([
+      subject.valid?
+      expect(subject.errors.full_messages).to match_array([
         "Name can't be blank",
         "User can't be blank",
         "User must exist"
@@ -13,17 +14,31 @@ describe Form do
     end
 
     it 'requires payload' do
-      form = Form.new
-      form.payload = nil
-      form.valid?
-      expect(form.errors.full_messages).to include("Payload can't be blank")
+      subject.payload = nil
+      subject.valid?
+      expect(subject.errors.full_messages).to include("Payload can't be blank")
     end
   end
 
   context 'defaults' do
     it 'sets the default payload' do
-      form = Form.new
-      expect(form.payload.present?).to be_truthy
+      expect(subject.payload.present?).to be_truthy
+    end
+  end
+
+  describe '#email_recipient' do
+    before { allow(subject).to receive(:user).and_return(double(email: 'user@test.com')) }
+    context 'when email_recipient is present' do
+      before { subject.email_recipient = 'form@test.com' }
+      it 'returns the email_recipient' do
+        expect(subject.email_recipient).to eq 'form@test.com'
+      end
+    end
+    context 'when email_recipient is not present' do
+      before { subject.email_recipient = nil }
+      it 'returns the user email' do
+        expect(subject.email_recipient).to eq 'user@test.com'
+      end
     end
   end
 end
