6f798b3e32725077f134801e782f5f390eb7a76f
Jason Adkison <jadkison@gmail.com>
2020-01-29 21:31:52 -0600

    create the customer if they dont exist

--- app/jobs/create_or_update_stripe_customer_for_user_job.rb
+++ app/jobs/create_or_update_stripe_customer_for_user_job.rb
@@ -1,11 +1,25 @@
 class CreateOrUpdateStripeCustomerForUserJob < ApplicationJob
   def perform(user_id)
     user = User.find(user_id)
-    if user.stripe_id.present?
-      Stripe::Customer.update(user.stripe_id, { email: user.email, name: user.full_name })
+    return create_stripe_customer_for_user(user) unless user.stripe_id.present?
+
+    update_stripe_customer_for_user(user)
+  end
+
+  private
+
+  def create_stripe_customer_for_user(user)
+    customer = Stripe::Customer.create(email: user.email, name: user.full_name)
+    user.update!(stripe_id: customer['id'])
+  end
+
+  def update_stripe_customer_for_user(user)
+    Stripe::Customer.update(user.stripe_id, { email: user.email, name: user.full_name })
+  rescue Stripe::InvalidRequestError => e
+    if e.message.include?("No such customer")
+      create_stripe_customer_for_user(user)
     else
-      customer = Stripe::Customer.create(email: user.email, name: user.full_name)
-      user.update!(stripe_id: customer['id'])
+      raise e
     end
   end
 end
