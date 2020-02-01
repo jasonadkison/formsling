0e60ba4a48acaeaf3890f3e7f6972431a8a6b47b
Jason Adkison <jadkison@gmail.com>
2020-02-01 14:20:14 -0600

    add user component to top of editor when clicking tool

--- app/javascript/components/FormEditor/Tools.js
+++ app/javascript/components/FormEditor/Tools.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { createElement } from 'react';
 import { useEditor } from '@craftjs/core';
 import cx from 'classnames';
 
-import resolvers from './resolvers';
+import Tool from './Tool';
 
 const Tools = () => {
-  const { connectors, enabled } = useEditor((state) => ({
+  const { enabled } = useEditor((state) => ({
     enabled: state.options.enabled,
   }));
 
@@ -13,76 +13,13 @@ const Tools = () => {
 
   return (
     <nav className={classNames}>
-      <a
-        className="button"
-        ref={ref => connectors.create(ref, <resolvers.Text />)}
-        title="Text"
-        data-tooltip="Text"
-      >
-        <span className="icon">
-          <i className="fas fa-keyboard" aria-hidden="true" />
-        </span>
-      </a>
-      <a
-        className="button"
-        ref={ref => connectors.create(ref, <resolvers.Dropdown />)}
-        title="Dropdown"
-        data-tooltip="Dropdown"
-      >
-        <span className="icon">
-          <i className="fas fa-list-alt" aria-hidden="true" />
-        </span>
-      </a>
-      <a
-        className="button"
-        ref={ref => connectors.create(ref, <resolvers.Date />)}
-        title="Date"
-        data-tooltip="Date"
-      >
-        <span className="icon">
-          <i className="fas fa-calendar-alt" aria-hidden="true" />
-        </span>
-      </a>
-      <a
-        className="button"
-        ref={ref => connectors.create(ref, <resolvers.Toggle />)}
-        title="Toggle"
-        data-tooltip="Toggle"
-      >
-        <span className="icon">
-          <i className="fas fa-check-square" aria-hidden="true" />
-        </span>
-      </a>
-      <a
-        className="button"
-        ref={ref => connectors.create(ref, <resolvers.Columns />)}
-        title="Columns"
-        data-tooltip="Columns"
-      >
-        <span className="icon">
-          <i className="fas fa-columns" aria-hidden="true" />
-        </span>
-      </a>
-      <a
-        className="button"
-        ref={ref => connectors.create(ref, <resolvers.Heading />)}
-        title="Heading"
-        data-tooltip="Heading"
-      >
-        <span className="icon">
-          <i className="fas fa-heading" aria-hidden="true" />
-        </span>
-      </a>
-      <a
-        className="button"
-        ref={ref => connectors.create(ref, <resolvers.Paragraph />)}
-        title="Paragraph"
-        data-tooltip="Paragraph"
-      >
-        <span className="icon">
-          <i className="fas fa-paragraph" aria-hidden="true" />
-        </span>
-      </a>
+      <Tool name="Text" icon={<i className="fas fa-keyboard" aria-hidden="true" />} />
+      <Tool name="Dropdown" icon={<i className="fas fa-list-alt" aria-hidden="true" />} />
+      <Tool name="Date" icon={<i className="fas fa-calendar-alt" aria-hidden="true" />} />
+      <Tool name="Toggle" icon={<i className="fas fa-check-square" aria-hidden="true" />} />
+      <Tool name="Columns" icon={<i className="fas fa-columns" aria-hidden="true" />} />
+      <Tool name="Heading" icon={<i className="fas fa-heading" aria-hidden="true" />} />
+      <Tool name="Paragraph" icon={<i className="fas fa-paragraph" aria-hidden="true" />} />
     </nav>
   );
 };
