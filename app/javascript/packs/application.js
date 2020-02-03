7b45b85ac7006dce697414cf42e8de4d85a1eb74
Jason Adkison <jadkison@gmail.com>
2020-02-02 18:05:46 -0600

    adds favicon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,8 +16,8 @@ import '../lib/close_notifications';
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
 // Support component names relative to this directory:
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
